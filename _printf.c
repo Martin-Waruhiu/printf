b65f019e830e04799fc5a8f5d51b652500e7262b
ndush <mdamarisnduku@gmail.com>
2023-07-24 08:19:26 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * _printf - function that produces output according to a format
+ *@format: first argument
+ *...: unknown variables
+ * Return: 0
+ */
+int _printf(const char *format, ...)
+{
+	int i;
+	int x;
+	int count = 0;
+	char *str;
+	va_list arg_list;
+
+	va_start(arg_list, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[0]);
+			count++;
+		}
+		else
+		{
+			format++;
+		switch (format[i])
+		{
+			case 'c':
+				_putchar( va_arg(arg_list, int));
+				count++;
+				break;
+			case 's':
+				str = va_arg(arg_list,  char*);
+				if (!str)
+					str = "(nil)";
+				for (x = 0 ; str[x] != '\0'; x++)
+					_putchar(str[x]);
+				count++;
+				break;
+			case '%':
+				_putchar('%');
+				count++;
+				break;
+			default:
+				break;
+		}
+		}
+	}
+	va_end(arg_list);
+	return (0);
+}
