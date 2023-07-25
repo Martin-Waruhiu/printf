c76f74528cf5341c620cb62bd74f282127cd21ea
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-07-25 05:17:23 -0700

    update

--- main.h
+++ main.h
@@ -4,21 +4,23 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+
+/**
+ * struct format - name of struct
+ * @specs: - Array of char pointer
+ * @func: pointer to function
+ */
+typedef struct format
+{
+	char *specs;
+	int (*func)();
+} spec_fun;
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int printf_s(va_list argz);
 int printf_c(va_list argz);
 int printf_percent(void);
-
-
-typedef struct format
-{
-	char *specs;
-	int (*func)();
-}spec_fun;
-
-
-
 
 
 #endif
