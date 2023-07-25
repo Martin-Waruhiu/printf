b080633fa6e02bfd9d6eb44c09e1ecf6f043975f
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-07-25 16:25:17 -0700

    width

---
+++ width.c
@@ -0,0 +1,47 @@
+get_width.c
+
+
+
+
+
+
+#include "main.h"
+
+
+/**
+ * get_width - find width
+ * @format: str in format
+ * @list: argss
+ * @i: argzz
+ *
+ * Return: width.
+ */
+int get_width(const char *format, int *i, va_list list)
+{
+	int c;
+	int wid = 0;
+
+
+	for (c = *i + 1; format[c] != '\0'; c++)
+	{
+		if (is_digit(format[c]))
+		{
+			wid *= 10;
+			wid += format[c] - '0';
+		}
+		else if (format[c] == '*')
+		{
+			c++;
+			wid = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+
+	*i = c - 1;
+
+
+	return (wid);
+}
