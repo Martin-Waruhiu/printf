fd12149295669d33e213b713234d85007c758f2a
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-07-25 17:10:40 -0700

    isdigit

---
+++ isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * is_digit - checks digit
+ * @c: characterrr
+ *
+ * Return: 1(success)  0 error
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+
+	return (0);
+}
